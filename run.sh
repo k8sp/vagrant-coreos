9e873503d0d8b7831cb547ded81e713ba0ea26f8
Yi Wang <yi.wang.2005@gmail.com>
2016-05-10 21:00:49 -0700

    Make run.sh consumable

--- run.sh
+++ run.sh
@@ -1,29 +1,39 @@
-
-# Download kubectl.
-if [[ `uname` == "Darwin" ]]; then 
-    curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.3/bin/darwin/amd64/kubectl
-else
-    curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.3/bin/linux/amd64/kubectl
+if [[ ! -f ./`basename $0` ]]; then
+    echo "Please run this program in the directory where it resides."
+    exit
 fi
-chmod +x kubectl
+
+# Download kubectl into the current directory if no kubectl installed yet.
+if [[ `which kubectl` == "" ]]; then
+    if [[ ! -f ./kubectl ]]; then
+	if [[ `uname` == "Darwin" ]]; then 
+	    curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.3/bin/darwin/amd64/kubectl
+	else
+	    curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.3/bin/linux/amd64/kubectl
+	fi
+	chmod +x kubectl
+    fi
+    export PATH=`pwd`:$PATH
+fi
 
 # Download Vagrant configurations.
-git clone https://github.com/coreos/coreos-kubernetes.git
-cd coreos-kubernetes/multi-node/vagrant
-
-# Use default config.p
-mv config.rb.sample config.rb
-
-# Make sure that we use the most recent CoreOS Vagrant boxes.
-vagrant box update
+if [[ ! -d coreos-kubernetes ]]; then
+    git clone https://github.com/coreos/coreos-kubernetes.git
+    (
+	cd coreos-kubernetes/multi-node/vagrant
+	mv config.rb.sample config.rb # Use default config.p
+	vagrant box update
+    )
+fi
 
 # Start the virtual cluster.
+cd coreos-kubernetes/multi-node/vagrant
 vagrant up
 
-# Configure kubectl
+# Configure kubectl.
 export KUBECONFIG="$(pwd)/kubeconfig"
 kubectl config use-context vagrant-multi
 
 echo "Waiting for the cluster to startup"
 sleep 10
-./kubectl get nodes
+kubectl get nodes
